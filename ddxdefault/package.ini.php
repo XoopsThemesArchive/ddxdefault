ce6578ab2d1bf3b6ddff4dd12c645ccf9283e8be
mambax7 <mambax7@gmail.com>
2015-05-05 08:57:16 -0400

    Original files

---
+++ ddxdefault/package.ini.php
@@ -0,0 +1,16 @@
+<?php
+/**
+
+[Manifesto]
+Name="Default Theme-DD(Drag and Drop)Version"
+Url="http://xoops.org and http://www.wanisys.net"
+Version="2.00"
+
+[Theme]
+Author=XOOPS Project Team and Wanikoo and Authors of rico and prototype
+ScreenShot="screenshot.png"
+Description="Default theme-DD(Drag and Drop)Version of XOOPS 2.0.X"
+W3C=NG
+
+*/
+?>
